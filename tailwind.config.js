ef4e650808170dc9780e285903fea18668e3fd07
gadylf <livlicadyl@gmail.com>
2025-07-23 11:18:08 +0000

    23-07-2025

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,7 +63,18 @@ module.exports = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		fontSize: {
+        "xs": "0.75rem",
+        "sm": "0.875rem",
+        "base": "1rem",
+        "lg": "1.125rem",
+        "xl": "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+      }
   	}
   },
   plugins: [require("tailwindcss-animate")],
